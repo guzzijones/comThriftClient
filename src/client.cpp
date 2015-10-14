c8448e2301fdd2d3d90b5b3449a8fe1f145b33d1
ajonen8940 <ajonen8940@vm4.defaultdomain>
2015-10-14 01:56:49 +0000

    first commit

---
+++ src/client.cpp
@@ -0,0 +1,27 @@
+#include "distData.h"  // As an example
+
+#include <transport/TSocket.h>
+#include <transport/TBufferTransports.h>
+#include <protocol/TBinaryProtocol.h>
+
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace comProto;
+
+int main(int argc, char **argv) {
+  boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+
+  distDataClient client(protocol);
+  transport->open();
+  client.ping();
+   
+ result feedBack;
+ client.runCmd(feedBack,"ls -al");
+  transport->close();
+
+  return 0;
+}
